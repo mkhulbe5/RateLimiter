29da99972e5d680656272e8bf1c527b0f1045610
mkhulbe5 <chandmohit163@gmail.com>
2025-06-01 11:19:51 +0530

    adding bean config

--- src/main/java/com/mohit/ratelimiter/RateLimiterProjectApplication.java
+++ src/main/java/com/mohit/ratelimiter/RateLimiterProjectApplication.java
@@ -1,10 +1,23 @@
 package com.mohit.ratelimiter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+import com.mohit.ratelimiter.interceptor.CalculationInterceptor;
 
 @SpringBootApplication
 public class RateLimiterProjectApplication {
+
+	@Autowired
+	@Lazy
+	private CalculationInterceptor interceptor;
+
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor).addPathPatterns("/api/calculator/**");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RateLimiterProjectApplication.class, args);
